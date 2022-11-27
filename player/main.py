7c5f557692ba51a780c2fb6a07940e9e761653d8
marius004 <scarlatmariusstefan2018@gmail.com>
2022-11-27 11:47:23 +0200

    replaced the hardcoded guess service with the http one

--- player/main.py
+++ player/main.py
@@ -1,11 +1,14 @@
 from services.buggy_feedback_service import BuggyFeedbackService
 from services.hardcoded_guess_service import HardcodedGuessService
+from services.http_guess_service import HttpGuessService
 from util import run_gui_mode, run_cli_mode
 from constants import DEFAULT_WORD
 from cli.parser import CLIParser
 from models.state import State
 
-guess_service = HardcodedGuessService()
+# for testing only
+# guess_service = HardcodedGuessService()
+guess_service = HttpGuessService()
 feedback_service = BuggyFeedbackService()
    
 if CLIParser().parse().gui:
