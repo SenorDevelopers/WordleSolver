5c10a2a86ba313b24995f6894265915346d65a54
SILVIANPC\silvi <silvianachim@hotmail.com>
2022-11-27 12:32:57 +0200

    -database update

--- api/Wordle-ASC/Model/UoW.cs
+++ api/Wordle-ASC/Model/UoW.cs
@@ -1,5 +1,7 @@
 ﻿using Database.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.Configuration;
 
 namespace Database;
@@ -8,6 +10,23 @@ public class UoW : DbContext
 {
 	public DbSet<Word> Words { get; set; } = null!;
 	public DbSet<Guess> Guesses { get; set; } = null!;
+
+	public UoW()
+	{
+		var databaseCreator = Database.GetService<IDatabaseCreator>() as IRelationalDatabaseCreator;
+		if (databaseCreator != null)
+		{
+			if (!databaseCreator.CanConnect())
+			{
+				databaseCreator.Create();
+			}
+
+			if (!databaseCreator.HasTables())
+			{
+				databaseCreator.CreateTables();
+			}
+		}
+	}
 
 	protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 	{
